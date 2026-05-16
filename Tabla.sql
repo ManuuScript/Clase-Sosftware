44a41d7b5d2c65282bb89e0c918f40a7ac126dc2
ManuuScript <angelmanuela925y@gmail.com>
2026-05-16 15:01:47 -0400

    Add files via upload

---
+++ Tabla.sql
@@ -0,0 +1,7 @@
+
+INSERT INTO clientes (nombre, apellido, direccion) VALUES
+('Angel', 'Perez', 'Barrio Santa Ana, San Francisco de Macorís'),
+('Diogenes', 'Amaury', 'Sector Vista al Valle, San Francisco de Macorís'),
+('Elio', 'Perez', 'Urbanización Toribio Piantini, Santo Domingo'),
+('Esperanza', 'Taveras', 'Sector Los Maestros, San Francisco de Macorís'),
+('Diego', 'Perez', 'Av. 27 de Febrero, Santiago');
